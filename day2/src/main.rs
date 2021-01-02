fee58857293b7084606dce2a21d8668a3d6bc0bb
Oliver Pieper <pieper@viaregio.de>
2021-01-02 18:07:57 +0100

    Modified to use filter(); part 2 of Day 2 added.
    Uses filter() and count() instead of the brute force counting in a for loop.
    This allowed for the easy addition of another predicate for part 2.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,27 +1,48 @@
+#[macro_use]
+extern crate lazy_static;
+
 use regex::Regex;
+
+fn conforms_to_rule_1(line: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^(\d+)\-(\d+) (.): (.+)$").unwrap();
+    }
+    let caps = RE.captures(line).unwrap();
+
+    let character_count = caps[4].matches(&caps[3]).count();
+    let min_repeats = caps[1].parse::<usize>().unwrap();
+    let max_repeats = caps[2].parse::<usize>().unwrap();
+
+    character_count >= min_repeats && character_count <= max_repeats
+}
+
+fn conforms_to_rule_2(line: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^(\d+)\-(\d+) (.): (.+)$").unwrap();
+    }
+    let caps = RE.captures(line).unwrap();
+
+    let pos1 = caps[1].parse::<usize>().unwrap();
+    let pos2 = caps[2].parse::<usize>().unwrap();
+    let character: char = caps[3].chars().next().unwrap();
+
+    let char_at_pos1 = caps[4].chars().nth(pos1 - 1).unwrap();
+    let char_at_pos2 = caps[4].chars().nth(pos2 - 1).unwrap();
+
+    ((char_at_pos1 == character) || (char_at_pos2 == character))
+        && !((char_at_pos1 == character) && (char_at_pos2 == character))
+}
 
 fn main() {
     let input = include_str!("input.txt").lines();
 
-    let re = Regex::new(r"^(\d+)\-(\d+) (.): (.+)$").unwrap();
+    println!(
+        "{} passwords conform to rule 1",
+        input.clone().filter(|x| conforms_to_rule_1(x)).count()
+    );
 
-    let mut count = 0;
-    for line in input {
-        println!("{}", line);
-        assert!(re.is_match(line));
-        let caps = re.captures(line).unwrap();
-        println!(
-            "are from {} to {} characters '{}' in \"{}\"?",
-            &caps[1], &caps[2], &caps[3], &caps[4]
-        );
-
-        let actual_number = caps[4].matches(&caps[3]).count();
-        println!("{}", actual_number);
-        if actual_number >= caps[1].parse::<usize>().unwrap()
-            && actual_number <= caps[2].parse::<usize>().unwrap()
-        {
-            count += 1;
-        }
-    }
-    println!("{} pwds match their rule.", count);
+    println!(
+        "{} passwords conform to rule 2",
+        input.filter(|x| conforms_to_rule_2(x)).count()
+    );
 }
